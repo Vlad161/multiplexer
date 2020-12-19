4027448f3cdf2a21eace3f563d2d9b73b91883cb
Vladislav Bagdasaryan <bagdasaryanvlad@gmail.com>
2020-12-19 23:27:57 +0300

    Added new line symbol in logs

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -15,10 +15,12 @@ func New() *logger {
 
 func (l logger) Info(format string, a ...interface{}) {
 	_, _ = io.WriteString(os.Stdout, fmt.Sprintf(format, a...))
+	_, _ = io.WriteString(os.Stdout, "\n")
 }
 
 func (l logger) Error(format string, a ...interface{}) {
 	_, _ = io.WriteString(os.Stderr, fmt.Sprintf(format, a...))
+	_, _ = io.WriteString(os.Stdout, "\n")
 }
 
 func (l logger) Fatal(format string, a ...interface{}) {
